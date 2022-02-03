6e9cfda07e843c04a285078e80ddbf00f1e64010
Julie <saruru1@naver.com>
2022-02-04 00:04:21 +0900

    - ReactHook 을 통해 함수컴포넌트에 state 생성하는 예제

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,31 +1,38 @@
 /**
  * ReactHook 을 사용하여 리팩토링하는 예제
  */
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 
-class SimpleHabit extends Component {
-    state = {
-        count: 0,
+const SimpleHabit = (props) => {
+
+    /**
+     * ReactHook 에서는 state 를 사용하기위해 useState API 를 사용한다.
+     * 아래와 같이 [count, setCount] 를 변수로 넣으면
+     * count 라는 state 와
+     * setCount 라는 state 를 변경하는 함수를 리턴받는다.
+     */
+    const [count, setCount] = useState(0);
+
+    /**
+     * 클래스에서 이벤트 부분은 멤버변수로 선언되어 할당되고
+     * ReactHook 에서는 더이상 this 를 사용하지 않고 멤버변수로 접근할 수 있게된다.
+     */
+    const handleIncrement = () => {
+        setCount(count + 1);
     }
 
-    handleIncrement = () => {
-        this.setState({count: this.state.count + 1});
-    }
-
-    render() {
-        return (
-            <li className="habit">
-                <span className="habit-name">Reading</span>
-                <span className="habit-count">{this.state.count}</span>
-                <button
-                    className="habit-button habit-increase"
-                    onClick={this.handleIncrement}
-                >
-                    <i className="fas fa-plus-square"></i>
-                </button>
-            </li>
-        );
-    }
+    return (
+        <li className="habit">
+            <span className="habit-name">Reading</span>
+            <span className="habit-count">{count}</span>
+            <button
+                className="habit-button habit-increase"
+                onClick={handleIncrement}
+            >
+                <i className="fas fa-plus-square"></i>
+            </button>
+        </li>
+    );
 }
 
 export default SimpleHabit;
